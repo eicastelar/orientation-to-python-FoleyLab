c20b7fbc307c8431a293806ad9e8383fed3b38c0
FoleyLab <jayfoleyiv@gmail.com>
2019-01-18 12:58:12 -0500

    Update day1.py

--- day1.py
+++ day1.py
@@ -5,18 +5,27 @@ Created on Thu Jan 17 16:27:09 2019
 
 @author: foleyj10
 
+
+
 TO-DO:
 (0) Create personal folders
 (1) Hello World!
 (2) Everyone commit! Everyone fetch!
-(3) Variable assingments and arthmetic operations (int, float, complex)
-(4) For Loops
+(3) Variables
+    (a)  mass
+    (b)  velocity
+    (c)  kinetic energy
+    (d)  charge
+    (e)  position 
+    (f)  separation
+    (g)  potential energy
+    
+(4) For Loops - compute kinetic energy for a bunch of particles
 (5) importing libraries - numpy and matplotlib.pyplot
 (6) numpy arrays
 (7) Functions on numpy arrays
 (8) Plotting results of numpy arrays with pyplot
-(9) A function to multiply matrices
-(10) The Liouville Equation for PIB with harmonic trap in PIB basis.
+
 
 """
 
